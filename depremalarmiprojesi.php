1cf6413e5a7847e37c56a2a27de852cdba8d7948
neslicinar1150-lgtm <neslicinar1150@gmail.com>
2026-06-27 09:32:50 +0300

    Update logging to use deprem_log.txt with timestamp

--- depremalarmiprojesi.php
+++ depremalarmiprojesi.php
@@ -1,4 +1,8 @@
 <?php
-file_put_contents("deprem.txt", "DEPREM " . date("H:i:s") . "\n", FILE_APPEND);
+file_put_contents(
+"deprem_log.txt",
+date("d.m.Y H:i:s")." Deprem algilandi\n",
+FILE_APPEND
+);
 echo "OK";
 ?>
